ff94da571fa5a0ef0b246ecde07492f7416a402e
Galo Navarro <galo@midokura.com>
2013-03-12 16:44:10 +0100

    git mergetool, setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s bash/bashrc ~/.bashrc
+ln -s vi/vim ~/.vim
+ln -s vi/vimrc ~/.vimrc
+ln -s tmux/tmuxrc ~/.tmuxrc
+ln -s screen/screenrc ~/.screenrc
+ln -s git/gitconfig ~/.gitconfig
