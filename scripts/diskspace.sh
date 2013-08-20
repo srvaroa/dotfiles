8a3dd1cc4f876991ab654f0618da1564857d713d
Galo Navarro <galo@midokura.com>
2013-08-20 23:06:03 +0200

    Free, not used disk space

--- scripts/diskspace.sh
+++ scripts/diskspace.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Usage: diskspace.sh <device> <limit> <email>
 #   will send an alert email to <email> if % free space in <device> goes
-#   over <limit>
+#   under <limit>
 FREE=`df $1 | tail -n1 | awk '{print $5}' | cut -d'%' -f1`
-if [ $FREE -ge $2 ]
+if [ $FREE -le $2 ]
 then
     S="Alert: out of space in $1, $FREE % ${hostname} ${date}"
     df -h | mail -s "$S" $3
