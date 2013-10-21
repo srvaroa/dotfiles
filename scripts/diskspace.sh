3cb576d52cf78a5d67927cc3ae3977b0bb530a7d
Galo Navarro <galo@midokura.com>
2013-10-21 20:02:59 +0200

    fix diskspace check

--- scripts/diskspace.sh
+++ scripts/diskspace.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Usage: diskspace.sh <device> <limit> <email>
-#   will send an alert email to <email> if % free space in <device> goes
-#   under <limit>
-FREE=`df $1 | tail -n1 | awk '{print $5}' | cut -d'%' -f1`
-if [ $FREE -le $2 ]
+#   will send an alert email to <email> if % used space in <device> goes
+#   above <limit>
+USED=`df $1 | tail -n1 | awk '{print $5}' | cut -d'%' -f1`
+if [ $USED -gt $2 ]
 then
-    S="Alert: out of space in $1, $FREE % ${hostname} ${date}"
+    S="Alert: running out of space in $1, $USED % ${hostname} ${date}"
     df -h | mail -s "$S" $3
 fi
