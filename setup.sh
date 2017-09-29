d3850246e051acac606917986756a103a5e32523
Galo Navarro <anglorvaroa@gmail.com>
2017-09-29 20:16:32 +0200

    Update
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ln -s "$PWD/bash/bashrc" ~/.bashrc
-ln -s "$PWD/vi/vim" ~/.vim
-ln -s "$PWD/vi/vimrc" ~/.vimrc
+ln -s "$PWD/.vim"
+ln -s "$PWD/.vim/vimrc" ~/.vimrc
 ln -s "$PWD/tmux/tmuxrc" ~/.tmuxrc
 ln -s "$PWD/screen/screenrc" ~/.screenrc
 ln -s "$PWD/git/gitconfig" ~/.gitconfig
