4e055d2dcbf8a11a34456f84255afdde0cc3bdf1
Galo Navarro <anglorvaroa@gmail.com>
2018-09-05 22:33:46 +0200

    setup.sh: don't link .gitignore

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ find . -maxdepth 1 -name '.*' | while read f
 do
     f=$(basename $f)
     dest=$HOME/$f
-    if [ "$f" != ".git" ] && [ ! -f "$dest" ] && [ ! -d "$dest" ]
+    if [ "$f" != ".git" ] && \
+       [ "$f" != ".gitignore" ] && \
+       [ ! -f "$dest" ] &&   \
+       [ ! -d "$dest" ]
     then
         ln -s $PWD/$f $dest
     fi
