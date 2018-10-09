12bd746856cef3a918ab4b73c68f4f9e70cfa971
Galo Navarro <anglorvaroa@gmail.com>
2018-10-09 23:44:38 +0200

    installer
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+function enable_services() {
+    sudo systemctl enable tlp.service
+}
+
+apt update
+apt -y upgrade
+apt install --no-install-recommends -y \
+    automake \
+    ca-certificates \
+    clipit u
+    curl \
+    feh \
+    fortune \
+    fortune-mod \
+    git \
+    gnupg \
+    htop \
+    jq \
+    msmtp \
+    mutt \
+    pinentry-curses \
+    powertop \
+    redshift-gtk \
+    sudo \
+    tree \
+    tlp \
+    vim
+
