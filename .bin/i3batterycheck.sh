e13f69b342338ff84ff41f712863adc55abcf185
Galo Navarro <anglorvaroa@gmail.com>
2019-08-31 11:18:00 +0200

    i3battcheck: add early warning
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- .bin/i3batterycheck.sh
+++ .bin/i3batterycheck.sh
@@ -2,17 +2,23 @@
 #
 # Rudimentary calculation of remaining battery life, and notification
 
-THRESHOLD_MINS=10
+WARNING_THRESHOLD_MINS=30
+CRITICAL_THRESHOLD_MINS=10
 
 if [ ! -z "$(cat /sys/class/power_supply/BAT0/uevent | grep -i discharging)" ]
 then
 	charge_now=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_CHARGE_NOW= | cut -f2 -d=)
 	current_now=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_CURRENT_NOW= | cut -f2 -d=)
 	remaining=$(echo "$charge_now * 60 / $current_now" | bc)
-	if [ "$remaining" -lt "$THRESHOLD_MINS" ]
-	then
 		remaining_hh=$(echo "$remaining / 60" | bc)
 		remaining_mm=$(echo "$remaining - ($remaining_hh * 60)" | bc)
-		notify-send -u critical -t 60000 "Low battery.  Remaining: ${remaining_hh}:${remaining_mm}"
+	if [ "$remaining" -lt "$WARNING_THRESHOLD_MINS" ]
+	then
+		notify-send -u normal -t 60000 \
+			"Battery level low: ${remaining_hh}:${remaining_mm} remaining"
+	elif [ "$remaining" -lt "$CRITICAL_THRESHOLD_MINS" ]
+	then
+		notify-send -u critical -t 60000 \
+			"Battery level critical: ${remaining_hh}:${remaining_mm} remaining"
 	fi
 fi
