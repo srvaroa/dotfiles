d0af3725e13da937ecf6f64ae706c51d17b10b4b
Galo Navarro <anglorvaroa@gmail.com>
2019-08-31 11:45:30 +0200

    i3battcheck: do not pester on warn
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

--- .bin/i3batterycheck.sh
+++ .bin/i3batterycheck.sh
@@ -5,20 +5,21 @@
 WARNING_THRESHOLD_MINS=30
 CRITICAL_THRESHOLD_MINS=10
 
+WARNING_TOKEN_FILE=/tmp/i3battcheck_warned
+
 if [ ! -z "$(cat /sys/class/power_supply/BAT0/uevent | grep -i discharging)" ]
 then
 	charge_now=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_CHARGE_NOW= | cut -f2 -d=)
 	current_now=$(cat /sys/class/power_supply/BAT0/uevent | grep POWER_SUPPLY_CURRENT_NOW= | cut -f2 -d=)
 	remaining=$(echo "$charge_now * 60 / $current_now" | bc)
-		remaining_hh=$(echo "$remaining / 60" | bc)
-		remaining_mm=$(echo "$remaining - ($remaining_hh * 60)" | bc)
-	if [ "$remaining" -lt "$WARNING_THRESHOLD_MINS" ]
+	if [ "$remaining" -lt "$WARNING_THRESHOLD_MINS" ] && [ ! -f "$WARNING_TOKEN_FILE" ]
 	then
 		notify-send -u normal -t 60000 \
-			"Battery level low: ${remaining_hh}:${remaining_mm} remaining"
+			"Battery level low: ${remaining} mins remaining"
+		touch $WARNING_TOKEN_FILE
 	elif [ "$remaining" -lt "$CRITICAL_THRESHOLD_MINS" ]
 	then
 		notify-send -u critical -t 60000 \
-			"Battery level critical: ${remaining_hh}:${remaining_mm} remaining"
+			"Battery level critical: ${remaining} mins remaining"
 	fi
 fi
